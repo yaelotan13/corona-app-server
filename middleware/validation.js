c175ae0dbb1bf9512e19986513240b6275bf4cc5
yael.lotan <yaelotan19@gmail.com>
2020-04-08 11:39:39 +0300

    fix(middleware/blacklist.js): return 200 also when is blacklist

--- middleware/validation.js
+++ middleware/validation.js
@@ -1,7 +1,7 @@
 function validation (req, res, next) {
   const [requestId, deviceId, validationStr] = req.body.decrypted;
 
-  if (validationStr !== 'valid') return res.status(401).end();
+  if (validationStr !== 'valid') return res.status(200).end();
 
   req.body._id = deviceId;
   console.log(`in validation, req.body._id is ${req.body._id}`);
